b5e3332d9a8125218dedca6f45176ca33c200857
Summer Rizzo <srainrizzo@gmail.com>
2019-12-10 11:23:24 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+
+    #Set breed of dog
+    def breed=(breed)
+        @breed = breed
+      end
+    #Get breed
+      def breed
+        @breed
+      end
+    #Set name of dog
+    def name=(name)
+      @name = name
+    end
+
+    #Get name
+    def name
+      @name
+    end
+  end
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+    #Set job of person
+    def job=(job)
+        @job = job
+      end
+    #Get job
+      def job
+        @job
+      end
+
+    #Set name of person
+    def name=(name)
+      @name = name
+    end
+
+    #Get name
+    def name
+      @name
+    end
+  end
